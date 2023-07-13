647dd724940c2d900a554e3c4e92043033be3bd9
Hadi Aladdin <hadi.aladdin@gmail.com>
2023-07-13 08:02:09 -0400

    Update cryptogenie.sql

--- cryptogenie.sql
+++ cryptogenie.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `angstron_cryptogenie`
+-- Database: `cryptogenie`
 --
 
 -- --------------------------------------------------------
